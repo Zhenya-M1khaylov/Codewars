3f3e84756b760b59207728112fea30537566396f
Zheniqq <yuheni.mikhaylov@gmail.com>
2022-05-29 13:43:27 +0300

    add 2 tasks (8 kyu)

---
+++ 8kyu/String repeat.js
@@ -0,0 +1,10 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+function repeatStr (n, s) {
+    let newString = ''
+    for(let i = 0; i < n; i++) {
+        newString += s
+    }
+    return newString
+}
+console.log(repeatStr(3, 'wtf'))
