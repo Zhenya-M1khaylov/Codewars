06b0cfadd95057f043cfab4dafab6772edf114cf
Zheniqq <yuheni.mikhaylov@gmail.com>
2022-10-19 00:02:55 +0300

    +2 tasks (7 kyu/8 kyu)

---
+++ 7kyu/Beginner Series #3 Sum of Numbers.js
@@ -0,0 +1,19 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+//
+//     Note: a and b are not ordered!
+
+
+function getSum(a,b){
+    let count = 0;
+    if (a == b) return a;
+    if(a < b){
+        for (; a <= b; a++){
+            count += a;
+        }
+    } else if (a > b){
+        for(; b <= a; b++){
+            count += b;
+        }
+    }
+    return count;
+}
