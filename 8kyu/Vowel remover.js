9eeeff925443af34b6c3e5479a11181ea71bcd80
Zheniqq <yuheni.mikhaylov@gmail.com>
2022-10-21 18:17:16 +0300

    +2 tasks

---
+++ 8kyu/Vowel remover.js
@@ -0,0 +1,16 @@
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+
+
+function shortcut (string) {
+    return string.replace(/[aeiou]/g, "");;
+}
+
+// or
+
+// function shortcut (string) {
+//     return string
+//         .split('')
+//         .filter(str => !'aeiou'.includes(str))
+//         .join('')
+// }
