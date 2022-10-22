296f7132d9abdbc8d86ae080da854707de925a65
Zheniqq <yuheni.mikhaylov@gmail.com>
2022-10-22 22:22:15 +0300

    +2

---
+++ 8kyu/Grasshopper - Terminal game combat function.js
@@ -0,0 +1,10 @@
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+
+function combat(health, damage) {
+    if (damage > health) {
+        return 0
+    } else {
+        return health - damage
+    }
+}
