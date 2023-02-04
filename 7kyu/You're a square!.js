192d82786ae0a5d6c3482320cc088eef29c245e8
Zheniqq <yuheni.mikhaylov@gmail.com>
2023-02-04 12:41:29 +0300

    +2 tasks

---
+++ 7kyu/You're a square!.js
@@ -0,0 +1,10 @@
+let isSquare = function(n){
+    if (n < 0) {
+        return false
+    }
+    if(Number.isInteger(Math.sqrt(n))){
+        return true;
+    }else{
+        return false;
+    }
+}
